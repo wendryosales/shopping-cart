3c039ad27157878d2983d39d83ccf58105e58f5c
Wendryo S <wendryo.sales@gmail.com>
2022-02-02 15:58:09 -0300

    Initial commit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function createProductImageElement(imageSource) {
   img.src = imageSource;
   return img;
 }
-
+const deletedps = 1;
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
